4bbce5360bd4d99d149ce5b10b424b1dfe4e12c9
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2003-10-24 07:33:51 +0000

    Enable/fix shallow-binding stuff.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@41 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/spentry.s
+++ lisp-kernel/spentry.s
@@ -42,7 +42,7 @@ define([jump_builtin],[
 	jump_fname()
 ])
 	
-
+/*define([USE_DEEP_BINDING])*/
 
 _spentry(jmpsym)
 	__(jump_fname())
@@ -121,7 +121,11 @@ local_label(_throw_all_values):
 	__(cmpri(cr1,imm4,0))
 	__(la tsp,-(fulltag_misc+8)(imm3))
 	__(beq cr0,local_label(_throw_dont_unbind))
+        ifdef([USE_DEEP_BINDING],[
 	__(bl _SPunbind_to)
+        ],[
+        __(bl _SPsvar_unbind_to)
+        ])
 local_label(_throw_dont_unbind):
 	__(add imm0,vsp,nargs)
 	__(cmpri(cr0,nargs,0))
@@ -178,7 +182,11 @@ local_label(_nthrowv_nextframe):
 	__(ldr(sp,catch_frame.csp(temp0)))
 	__(beq cr0,local_label(_nthrowv_dont_unbind))
 	__(mflr loc_pc)
+        ifdef([USE_DEEP_BINDING],[
 	__(bl _SPunbind_to)
+        ],[
+        __(bl _SPsvar_unbind_to)
+        ])
 	__(mtlr loc_pc)
 local_label(_nthrowv_dont_unbind):
 	__(beq cr7,local_label(_nthrowv_do_unwind))
@@ -285,7 +293,11 @@ local_label(_nthrow1v_nextframe):
 	__(ldr(sp,catch_frame.csp(temp0)))
 	__(beq cr0,local_label(_nthrow1v_dont_unbind))
 	 __(mflr loc_pc)
+        ifdef([USE_DEEP_BINDING],[
 	 __(bl _SPunbind_to)
+        ],[
+         __(bl _SPsvar_unbind_to)
+        ])
 	 __(mtlr loc_pc)
 local_label(_nthrow1v_dont_unbind):
 	__(beq cr7,local_label(_nthrow1v_do_unwind))
@@ -4162,7 +4174,6 @@ _spentry(mvpasssym)
 	__(mtlr loc_pc)
 	__(jump_fname())
 
-define([USE_DEEP_BINDING])
         
 /* on entry:  temp0 = svar.  On exit, arg_z = value (possibly unbound_marker),
         arg_y = symbol, imm3 = svar.index */
@@ -4217,7 +4228,7 @@ _spentry(svar_bind)
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
         __(ldr(arg_y,svar.symbol(temp0)))
         __(treqi(imm3,0))              /* no real tlb index */
-        __(trllt(imm3,imm0))           /* tlb too small */
+        __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(vpush(temp1))
@@ -4238,7 +4249,7 @@ _spentry(svar_bind_self)
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
         __(ldr(arg_y,svar.symbol(temp0)))
         __(treqi(imm3,0))              /* no real tlb index */
-        __(trllt(imm3,imm0))           /* tlb too small */
+        __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(cmpri(temp1,no_thread_local_binding_marker))
@@ -4265,7 +4276,7 @@ _spentry(svar_bind_nil)
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
         __(ldr(arg_z,svar.symbol(temp0)))
         __(treqi(imm3,0))              /* no real tlb index */
-        __(trllt(imm3,imm0))           /* tlb too small */
+        __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(li imm0,nil_value)
@@ -4288,7 +4299,7 @@ _spentry(svar_bind_self_boundp_check)
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
         __(ldr(arg_y,svar.symbol(temp0)))
         __(treqi(imm3,0))              /* no real tlb index */
-        __(trllt(imm3,imm0))           /* tlb too small */
+        __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(cmpri(temp1,no_thread_local_binding_marker))
@@ -4327,7 +4338,7 @@ _spentry(svar_unbind_n)
 1:      __(subi imm0,imm0,1)
         __(ldr(temp0,binding.sym(imm1)))
         __(ldr(temp1,binding.val(imm1)))
-        __(cmpri(imm0,1))
+        __(cmpri(imm0,0))
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm1,binding.link(imm1)))
         __(strx(temp1,imm2,imm3))
@@ -4462,25 +4473,24 @@ _spentry(svar_progvsave)
         __(ldr(imm4,tcr.tlb_pointer(rcontext)))
         __(ldr(imm3,tcr.tlb_limit(rcontext)))
 3:
+        __(cmpri(cr1,arg_z,nil_value))
 	__(_car(temp0,temp4))
         __(ldr(imm0,svar.idx(temp0)))
         __(ldr(temp1,svar.symbol(temp0)))
 	__(_cdr(temp4,temp4))
-	__(_car(temp2,arg_z))
+        __(li temp2,unbound_marker)
+        __(trlle(imm3,imm0))
         __(ldrx(temp3,imm4,imm0))
-        __(trllt(imm3,imm0))
-	__(_cdr(arg_z,arg_z))
-        __(cmpri(cr1,temp3,no_thread_local_binding_marker))
 	__(cmpri(cr0,temp4,nil_value))
-        __(bne cr1,4f)
-        __(ldr(temp3,symbol.vcell(temp1)))
-4:              
-	__(push(temp2,imm2))
+        __(beq cr1,4f)
+	__(_car(temp2,arg_z))
+	__(_cdr(arg_z,arg_z))
+4:      __(push(temp2,imm2))
 	__(push(temp0,imm2))
 	__(push(imm1,imm2))
-	__(mr imm1,imm2)
+        __(strx(temp2,imm4,imm0))
 	__(bne cr0,3b)
-	__(str(imm1,tcr.db_link(rcontext)))
+	__(str(imm2,tcr.db_link(rcontext)))
 	__(blr)
         ])
                 
