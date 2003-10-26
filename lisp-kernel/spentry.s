e1e89f5dd4b545483c3e50682fe8cafeadbf63aa
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2003-10-26 10:10:24 +0000

    Check for non-bindable symbols without trapping.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@46 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/spentry.s
+++ lisp-kernel/spentry.s
@@ -42,8 +42,6 @@ define([jump_builtin],[
 	jump_fname()
 ])
 	
-/*define([USE_DEEP_BINDING])*/
-
 _spentry(jmpsym)
 	__(jump_fname())
         
@@ -121,11 +119,7 @@ local_label(_throw_all_values):
 	__(cmpri(cr1,imm4,0))
 	__(la tsp,-(fulltag_misc+8)(imm3))
 	__(beq cr0,local_label(_throw_dont_unbind))
-        ifdef([USE_DEEP_BINDING],[
-	__(bl _SPunbind_to)
-        ],[
         __(bl _SPsvar_unbind_to)
-        ])
 local_label(_throw_dont_unbind):
 	__(add imm0,vsp,nargs)
 	__(cmpri(cr0,nargs,0))
@@ -182,11 +176,7 @@ local_label(_nthrowv_nextframe):
 	__(ldr(sp,catch_frame.csp(temp0)))
 	__(beq cr0,local_label(_nthrowv_dont_unbind))
 	__(mflr loc_pc)
-        ifdef([USE_DEEP_BINDING],[
-	__(bl _SPunbind_to)
-        ],[
         __(bl _SPsvar_unbind_to)
-        ])
 	__(mtlr loc_pc)
 local_label(_nthrowv_dont_unbind):
 	__(beq cr7,local_label(_nthrowv_do_unwind))
@@ -293,11 +283,7 @@ local_label(_nthrow1v_nextframe):
 	__(ldr(sp,catch_frame.csp(temp0)))
 	__(beq cr0,local_label(_nthrow1v_dont_unbind))
 	 __(mflr loc_pc)
-        ifdef([USE_DEEP_BINDING],[
-	 __(bl _SPunbind_to)
-        ],[
          __(bl _SPsvar_unbind_to)
-        ])
 	 __(mtlr loc_pc)
 local_label(_nthrow1v_dont_unbind):
 	__(beq cr7,local_label(_nthrow1v_do_unwind))
@@ -4178,11 +4164,6 @@ _spentry(mvpasssym)
 /* on entry:  temp0 = svar.  On exit, arg_z = value (possibly unbound_marker),
         arg_y = symbol, imm3 = svar.index */
 _spentry(svar_specref)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(b _SPspecref)
-        ],[
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
@@ -4194,14 +4175,8 @@ _spentry(svar_specref)
         __(bnelr)
 1:     	__(ldr(arg_z,symbol.vcell(arg_y)))
         __(blr)
-        ])
 
 _spentry(svar_specrefcheck)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(b _SPspecrefcheck)
-        ],[
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
@@ -4214,165 +4189,145 @@ _spentry(svar_specrefcheck)
 1:     	__(ldr(arg_z,symbol.vcell(arg_y)))
 2:      __(treqi(arg_z,unbound_marker))
         __(blr)
-        ])
 
 /* This never affects the symbol's vcell */
 _spentry(svar_bind)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(b _SPbind)
-        ],[
+0:              
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(treqi(imm3,0))              /* no real tlb index */
+        __(cmpri(imm3,0))
         __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
+        __(beq 9f)
         __(vpush(temp1))
-        __(vpush(temp0))
+        __(vpush(imm3))
         __(vpush(imm1))
         __(strx(arg_z,imm2,imm3))
         __(str(vsp,tcr.db_link(rcontext)))
         __(blr)
-        ])
+9:
+        __(ldr(arg_z,svar.symbol(temp0)))
+        __(lwi(arg_y,XSYMNOBIND))
+        __(set_nargs(2))
+        __(b _SPksignalerr)
 
 _spentry(svar_bind_self)
-        ifdef([USE_DEEP_BINDING],[
-        __(ldr(arg_z,svar.symbol(temp0)))
-        __(b _SPbind_self)
-        ],[
+0:              
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(treqi(imm3,0))              /* no real tlb index */
+        __(cmpri(imm3,0))
         __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
-        __(cmpri(temp1,no_thread_local_binding_marker))
+        __(cmpri(cr1,temp1,no_thread_local_binding_marker))
+        __(ldr(arg_y,svar.symbol(temp0)))
+        __(beq 9f)
         __(mr arg_z,temp1)
-        __(bne 1f)
+        __(bne cr1,1f)
         __(ldr(arg_z,symbol.vcell(arg_y)))
 1:              
         __(vpush(temp1))
-        __(vpush(temp0))
+        __(vpush(imm3))
         __(vpush(imm1))
         __(strx(arg_z,imm2,imm3))
         __(str(vsp,tcr.db_link(rcontext)))
         __(blr)
-        ])
+9:      __(ldr(arg_z,svar.symbol(temp0)))
+        __(lwi(arg_y,XSYMNOBIND))
+        __(set_nargs(2))
+        __(b _SPksignalerr)
 
 _spentry(svar_bind_nil)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_z,svar.symbol(temp0)))
-        __(b _SPbind_nil)
-        ],[
+0:              
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
+        __(cmpri(imm3,0))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
-        __(ldr(arg_z,svar.symbol(temp0)))
-        __(treqi(imm3,0))              /* no real tlb index */
+        __(beq- 9f)
         __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(li imm0,nil_value)
         __(vpush(temp1))
-        __(vpush(temp0))
+        __(vpush(imm3))
         __(vpush(imm1))
         __(strx(imm0,imm2,imm3))
         __(str(vsp,tcr.db_link(rcontext)))
         __(blr)
-        ])
+9:      __(ldr(arg_z,svar.symbol(temp0)))
+        __(lwi(arg_y,XSYMNOBIND))
+        __(set_nargs(2))
+        __(b _SPksignalerr)
                 	
 _spentry(svar_bind_self_boundp_check)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_z,svar.symbol(temp0)))
-        __(b _SPbind_self_boundp_check)
-        ],[
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(treqi(imm3,0))              /* no real tlb index */
+        __(cmpri(imm3,0))
         __(trlle(imm0,imm3))           /* tlb too small */
         __(ldrx(temp1,imm2,imm3))
         __(ldr(imm1,tcr.db_link(rcontext)))
+        __(beq 9f)              /* no real tlb index */
         __(cmpri(temp1,no_thread_local_binding_marker))
         __(mr arg_z,temp1)
         __(bne 1f)
+        __(ldr(arg_y,svar.symbol(temp0)))
         __(ldr(arg_z,symbol.vcell(arg_y)))
 1:      __(treqi(arg_z,unbound_marker))       
         __(vpush(temp1))
-        __(vpush(temp0))
+        __(vpush(imm3))
         __(vpush(imm1))
         __(strx(arg_z,imm2,imm3))
         __(str(vsp,tcr.db_link(rcontext)))
         __(blr)
-	])
+9:      __(ldr(arg_z,svar.symbol(temp0)))
+        __(lwi(arg_y,XSYMNOBIND))
+        __(set_nargs(2))
+        __(b _SPksignalerr)
+
 _spentry(svar_unbind)
-        ifdef([USE_DEEP_BINDING],[
-        __(b _SPunbind)
-        ],[
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))   
-        __(ldr(temp0,binding.sym(imm1)))
+        __(ldr(imm3,binding.sym(imm1)))
         __(ldr(temp1,binding.val(imm1)))
-        __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm1,binding.link(imm1)))
         __(strx(temp1,imm2,imm3))
         __(str(imm1,tcr.db_link(rcontext)))
         __(blr)
-        ])
 
 _spentry(svar_unbind_n)
-        ifdef([USE_DEEP_BINDING],[
-        __(b _SPunbind_n)
-        ],[
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))   
 1:      __(subi imm0,imm0,1)
-        __(ldr(temp0,binding.sym(imm1)))
+        __(ldr(imm3,binding.sym(imm1)))
         __(ldr(temp1,binding.val(imm1)))
         __(cmpri(imm0,0))
-        __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm1,binding.link(imm1)))
         __(strx(temp1,imm2,imm3))
         __(bne 1b)
         __(str(imm1,tcr.db_link(rcontext)))
         __(blr)
-        ])
 
  /*
    Clobbers imm1,imm2,imm5,arg_x, arg_y
 */
 _spentry(svar_unbind_to)
-        ifdef([USE_DEEP_BINDING],[
-        __(b _SPunbind_to)
-        ],[
         __(ldr(imm1,tcr.db_link(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
-1:      __(ldr(arg_x,binding.sym(imm1)))
+1:      __(ldr(imm5,binding.sym(imm1)))
         __(ldr(arg_y,binding.val(imm1)))
         __(ldr(imm1,binding.link(imm1)))
-        __(ldr(imm5,svar.idx(arg_x)))
         __(cmpr(imm0,imm1))
         __(strx(arg_y,imm2,imm5))
         __(bne 1b)
         __(str(imm1,tcr.db_link(rcontext)))
         __(blr)
-        ])
+	
 /* temp0 = svar for special symbol, arg_z = new value. */        
 _spentry(svar_specset)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(b _SPspecset)
-        ],[
         __(ldr(imm3,svar.idx(temp0)))
         __(ldr(imm0,tcr.tlb_limit(rcontext)))
         __(ldr(imm2,tcr.tlb_pointer(rcontext)))
@@ -4386,15 +4341,9 @@ _spentry(svar_specset)
         __(blr)
 1:     	__(str(arg_z,symbol.vcell(arg_y)))
         __(blr)
-        ])
 
 
 _spentry(svar_setqsym)
-        ifdef([USE_DEEP_BINDING],[
-        __(trap_unless_typecode_equal(temp0,subtag_svar,imm5))
-        __(ldr(arg_y,svar.symbol(temp0)))
-        __(b _SPsetqsym)
-        ],[
         __(ldr(arg_y,svar.symbol(temp0)))
 	__(ldr(imm0,symbol.flags(arg_y)))
 	__(andi. imm0,imm0,sym_vbit_const_mask)
@@ -4403,25 +4352,8 @@ _spentry(svar_setqsym)
 	__(lwi(arg_y,XCONST))
 	__(set_nargs(2))
 	__(b _SPksignalerr)
-        ])
 
 _spentry(svar_progvsave)
-        ifdef([USE_DEEP_BINDING],[
-        /* we should probably copy the list of svars in
-           arg_y to a list of symbols, but it's easier
-           just to bash the list in place, and this is
-           just a short-term kludge anyway.
-        */
-        __(mr temp0,arg_y)
-        __(b 1f)
-0:      __(_car(temp1,temp0))
-        __(ldr(temp1,svar.symbol(temp1)))
-        __(rplaca(temp0,temp1))
-        __(_cdr(temp0,temp0))
-1:      __(cmpri(temp0,nil_value))
-        __(bne 0b)
-        __(b _SPprogvsave)
-        ],[
 	/* Error if arg_z isn't a proper list.  That's unlikely,
 	   but it's better to check now than to crash later.
 	*/
@@ -4476,35 +4408,30 @@ _spentry(svar_progvsave)
         __(cmpri(cr1,arg_z,nil_value))
 	__(_car(temp0,temp4))
         __(ldr(imm0,svar.idx(temp0)))
-        __(ldr(temp1,svar.symbol(temp0)))
 	__(_cdr(temp4,temp4))
-        __(li temp2,unbound_marker)
         __(trlle(imm3,imm0))
         __(ldrx(temp3,imm4,imm0))
 	__(cmpri(cr0,temp4,nil_value))
+        __(li temp2,unbound_marker)
         __(beq cr1,4f)
 	__(_car(temp2,arg_z))
 	__(_cdr(arg_z,arg_z))
 4:      __(push(temp2,imm2))
-	__(push(temp0,imm2))
+	__(push(imm0,imm2))
 	__(push(imm1,imm2))
         __(strx(temp2,imm4,imm0))
+	__(mr imm1,imm2)
 	__(bne cr0,3b)
 	__(str(imm2,tcr.db_link(rcontext)))
 	__(blr)
-        ])
                 
 _spentry(svar_progvrestore)
-        ifdef([USE_DEEP_BINDING],[
-        __(b _SPprogvrestore)
-        ],[
 	__(ldr(imm0,tsp_frame.backlink(tsp)))	/* ignore .SPnthrowXXX values frame */
 	__(ldr(imm0,tsp_frame.data_offset(imm0)))
 	__(cmpri(cr0,imm0,0))
 	__(unbox_fixnum(imm0,imm0))
 	__(bne+ cr0,_SPsvar_unbind_n)
 	__(blr)
-        ])
                         
 /*  EOF, basically */
         .globl _SPsp_end
