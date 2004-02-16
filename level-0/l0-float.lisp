dddc4b388b88eaee58f558828f5d3adf713c740c
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2004-02-16 10:41:04 +0000

    ACOS of DOUBLE-FLOAT wants to (%MAKE-DFLOAT) the result.  Again.  Still.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@547 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-0/l0-float.lisp
+++ level-0/l0-float.lisp
@@ -699,7 +699,7 @@
      (locally (declare (type double-float x))
        (if (and (<= -1.0d0 x)
 		(<= x 1.0d0))
-	 (%double-float-acos! x (%make-sfloat))
+	 (%double-float-acos! x (%make-dfloat))
 	 (- double-float-half-pi (asin x)))))
     ((short-float rational)
      (ppc32::with-stack-short-floats ((sx x))
