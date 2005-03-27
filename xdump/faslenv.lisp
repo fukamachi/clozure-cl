de7b23b5f4411a8f0e9d1adb8af74aaeba54450a
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-03-27 21:36:45 +0000

    Increment min fasl version.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@1051 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- xdump/faslenv.lisp
+++ xdump/faslenv.lisp
@@ -59,7 +59,7 @@
 (defconstant $fasl-file-id #xff00)
 (defconstant $fasl-file-id1 #xff01)
 (defconstant $fasl-vers #x3e)
-(defconstant $fasl-min-vers #x3d)
+(defconstant $fasl-min-vers #x3e)
 (defconstant $faslend #xff)
 (defconstant $fasl-buf-len 2048)
 (defmacro deffaslop (n arglist &body body)
