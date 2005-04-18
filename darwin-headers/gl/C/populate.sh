7a03c403cacc8a151b62309aff9255e96d7373ef
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-04-18 06:40:41 +0000

    Process agl.h as well.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@1144 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- darwin-headers/gl/C/populate.sh
+++ darwin-headers/gl/C/populate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 h-to-ffi.sh /System/Library/Frameworks/OpenGL.framework/Headers/OpenGL.h
 h-to-ffi.sh /System/Library/Frameworks/GLUT.framework/Headers/glut.h
+h-to-ffi.sh /System/Library/Frameworks/AGL.framework/Headers/agl.h
