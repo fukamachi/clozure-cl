658ce6d9b438ef256c79949fa83e5a3b958288e3
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-05-17 10:44:41 +0000

    um, 050517b.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@1456 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -17,7 +17,7 @@
 (defparameter *openmcl-major-version* 0)
 (defparameter *openmcl-minor-version* 14)
 (defparameter *openmcl-revision* 3)
-(defparameter *openmcl-suffix* "050517")
+(defparameter *openmcl-suffix* "050517b")
 
 (defparameter *openmcl-version* (format nil "(Beta: ~~A) ~d.~d~@[.~d~]~@[-~a~]"
 					*openmcl-major-version*
