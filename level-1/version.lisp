c98344ef8d8f092c96ad2481e2f423bece104f72
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-07-03 01:57:42 +0000

    Lisp image du jour.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@1893 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -17,7 +17,7 @@
 (defparameter *openmcl-major-version* 0)
 (defparameter *openmcl-minor-version* 14)
 (defparameter *openmcl-revision* 3)
-(defparameter *openmcl-suffix* "050630")
+(defparameter *openmcl-suffix* "050702")
 (defparameter *openmcl-dev-level*
   #+ppc64-target "Alpha" #-ppc64-target "Beta")
 
