b2f2c0b6d39e40bae5144e9e83853d520517e776
bryan <bryan@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-08-15 20:00:36 +0000

    defclass: signal a program-error if invalid class option seen.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2286 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lib/macros.lisp
+++ lib/macros.lisp
@@ -1739,7 +1739,7 @@ are no Forms, OR returns NIL."
                    (:documentation
                     `(:documentation ',(cadr option)))
                    (t
-                    (list `',option-name `',(cdr option))))))
+                    (signal-program-error "Invalid class option: " option-name)))))
              (canonicalize-slot-spec (slot)
                (if (null slot) (signal-program-error "Illegal slot NIL"))
                (if (not (listp slot)) (setq slot (list slot)))
