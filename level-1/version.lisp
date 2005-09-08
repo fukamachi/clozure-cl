2f5c4b947d223bfcbc3aa2f84299fbb8e9662d00
bryan <bryan@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-09-08 08:12:47 +0000

    bump.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2532 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 0)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "pre-050906b")
+(defparameter *openmcl-suffix* "pre-050907")
 (defparameter *openmcl-dev-level*
   #+ppc64-target "Beta" #-ppc64-target nil)
 
