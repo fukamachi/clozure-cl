da1b2e6a3a524424ccc38a04310b2687b313afcc
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-09-24 22:05:16 +0000

    Bump.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2586 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 0)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "rc1-050922")
+(defparameter *openmcl-suffix* "rc1-050924")
 (defparameter *openmcl-dev-level*
   #+ppc64-target "Beta" #-ppc64-target nil)
 
