362ea2c31f7f9e6e88017c50dd7d5558ebe557bb
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-10-23 05:49:32 +0000

    ABI version change.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2631 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- xdump/heap-image.lisp
+++ xdump/heap-image.lisp
@@ -86,7 +86,7 @@
 			   (+ 4095 (file-position f)))))
 
 
-(defparameter *image-abi-version* 1003)
+(defparameter *image-abi-version* 1004)
 
 (defun write-image-file (pathname image-base spaces &optional (abi-version *image-abi-version*))
   (target-setup-image-header-sizes)
