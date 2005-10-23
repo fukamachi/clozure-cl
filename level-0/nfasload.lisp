5cd8b56f21292cf2ca6b5ad12660157a0cbe0c8b
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-10-23 07:41:38 +0000

    Remove the crude bootstrapping hack (at least the most recent one ...)
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2634 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-0/nfasload.lisp
+++ level-0/nfasload.lisp
@@ -931,8 +931,6 @@
 ;;; if the %fasload call fails, the lisp should exit (instead of
 ;;; repeating the process endlessly ...
 
-(eval-when (:compile-toplevel)
-  (warn "Remember to remove horrible bootstrapping hack here"))
 
 (defvar %toplevel-function%
   #'(lambda ()
@@ -940,8 +938,6 @@
                         *xload-cold-load-documentation*
                         *xload-startup-file*))
       (%set-tcr-toplevel-function (%current-tcr) nil) ; should get reset by l1-boot.
-      ;; Don't ask.
-      (setq *ALL-METERED-FUNCTIONS* %CLOSURE-CODE%)
       ;; Need to make %ALL-PACKAGES-LOCK% early, so that we can casually
       ;; do SET-PACKAGE in cold load functions.
       (setq %all-packages-lock% (make-read-write-lock))
