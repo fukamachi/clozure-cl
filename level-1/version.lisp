0ef97514f4f32b6af4dde537d739885ca0e98166
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-10-29 21:58:04 +0000

    Bump to 051029b.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2732 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 1)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "pre-051029")
+(defparameter *openmcl-suffix* "pre-051029b")
 (defparameter *openmcl-dev-level*
   #+ppc64-target "Beta" #-ppc64-target nil)
 
