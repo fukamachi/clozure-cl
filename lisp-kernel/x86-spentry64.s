4a042e4b992e6f7f718afd5d79deec77c545ba8a
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-11-08 11:46:25 +0000

    Start to actually write some code; still need more infrastructure.  On a 1 to 10 complexity scale, .SPbuiltin_plus is probably a 5 or 6; will need to be careful about register usage when consing bignums ('cause there aren't enough registers.  Stop me if you've heard this one.)
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2812 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/x86-spentry64.s
+++ lisp-kernel/x86-spentry64.s
@@ -13,3 +13,40 @@
    The LLGPL is also available online at
    http://opensource.franz.com/preamble.html
 */
+
+		
+	include(lisp.s)
+	_beginfile
+	
+        .align 2
+define([_spentry],[ifdef([__func_name],[_endfn],[])
+	_exportfn(_SP$1)
+	.line  __line__
+])
+
+             
+define([_endsubp],[
+	_endfn(_SP$1)
+# __line__
+])
+
+/* %arg_z <- %arg_y + %arg_z.  Do the fixnum case - including overflow -
+  inline.  Call out otherwise. */
+_spentry(builtin_plus)
+	__(movb %arg_z_b,%imm0_b)
+	__(orb %arg_y_b,%imm0_b)
+	__(testb $fixnummask,%imm0_b)
+	__(jne 2f)
+	__(addq %arg_y,%arg_z)
+	__(jo 1f)
+	__(jmp *%ra0)
+	/* Overflowed by one bit.  Make a bignum with 2 (32-bit) digits. */
+1:	__(unbox_fixnum(%arg_z,%imm0))
+	__(xorq overflow_mask(%rip),%imm0)
+	__(rol $32,%imm0)
+	
+2:
+_endsubp
+overflow_mask:	
+	.quad 0xe000000000000000
+		
