cba94b45c43b6054e603fe6fb6ddcebd49e6c4d9
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-11-14 09:01:34 +0000

    New files.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@2847 8bdb8f1d-59ce-db11-9e41-0016172a54ae

---
+++ lisp-kernel/x86-exceptions.h
@@ -0,0 +1,25 @@
+/*
+   Copyright (C) 2005 Clozure Associates
+   This file is part of OpenMCL.  
+
+   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+   License , known as the LLGPL and distributed with OpenMCL as the
+   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+   which is distributed with OpenMCL as the file "LGPL".  Where these
+   conflict, the preamble takes precedence.  
+
+   OpenMCL is referenced in the preamble as the "LIBRARY."
+
+   The LLGPL is also available online at
+   http://opensource.franz.com/preamble.html
+*/
+
+
+#ifdef LINUX
+#ifdef X8664
+#define xpGPRvector(x) ((natural *)(&((x)->uc_mcontext)))
+#define xpGPR(x,gprno) (xpGPRvector(x)[gprno])
+#define set_xpGPR(x,gpr,new) xpGPR((x),(gpr)) = (natural)(new)
+#define xpPC(x) ((x)->uc_mcontext.rip)
+#endif
+#endif
