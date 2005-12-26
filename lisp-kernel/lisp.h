6a5568bcfb01f97836652995ed19ffabd7871748
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-12-26 08:13:30 +0000

    Declare page_size, log2_page_size.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3001 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/lisp.h
+++ lisp-kernel/lisp.h
@@ -32,6 +32,8 @@
 Boolean use_mach_exception_handling;
 Boolean running_under_rosetta;
 
+int page_size, log2_page_size;
+
 static inline unsigned long
 _align_to_power_of_2(unsigned long n, unsigned power)
 {
