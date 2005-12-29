40c8a52f815a5a2c9ffdc471f49422f88c7610fe
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2005-12-29 11:36:43 +0000

    Bump ABI_VERSION_MIN.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3043 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/image.h
+++ lisp-kernel/image.h
@@ -89,6 +89,6 @@ load_openmcl_image(int, openmcl_image_file_header*);
 
 
 
-#define ABI_VERSION_MIN 1004
+#define ABI_VERSION_MIN 1005
 #define ABI_VERSION_CURRENT 1005
 #define ABI_VERSION_MAX 1005
