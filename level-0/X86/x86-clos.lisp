a3722d922d7ad51c4d8bcd8a0c2401b6b90599ed
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-01-10 12:50:32 +0000

    initial checkin
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3148 8bdb8f1d-59ce-db11-9e41-0016172a54ae

---
+++ level-0/X86/x86-clos.lisp
@@ -0,0 +1,237 @@
+;;;-*- Mode: Lisp; Package: CCL -*-
+;;;
+;;;   Copyright (C) 2006, Clozure Associates and contributors
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+(in-package "CCL")
+
+;;; It's easier to keep this is LAP; we want to play around with its
+;;; constants.
+
+;;; This just maps a SLOT-ID to a SLOT-DEFINITION or NIL.
+;;; The map is a vector of (UNSIGNED-BYTE 8); this should
+;;; be used when there are less than 255 slots in the class.
+(defppclapfunction %small-map-slot-id-lookup ((slot-id arg_z))
+  (ldr temp1 'map nfn)
+  (svref arg_x slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (header-length imm3 imm0)
+  (ldr temp0 'table nfn)
+  (cmplr arg_x imm3)
+  (srri imm0 arg_x target::word-shift)
+  (la imm0 target::misc-data-offset imm0)
+  (li imm1 target::misc-data-offset)
+  (bge @have-scaled-table-index)
+  (lbzx imm1 temp1 imm0)
+  (slri imm1 imm1 target::word-shift)
+  (la imm1 target::misc-data-offset imm1)
+  @have-scaled-table-index
+  (ldrx arg_z temp0 imm1)
+  (blr))
+
+;;; The same idea, only the map is a vector of (UNSIGNED-BYTE 32).
+(defppclapfunction %large-map-slot-id-lookup ((slot-id arg_z))
+  (ldr temp1 'map nfn)
+  (svref arg_x slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (header-length imm3 imm0)
+  (ldr temp0 'table nfn)
+  (cmplr arg_x imm3)
+  #+ppc64-target
+  (progn
+    (srdi imm0 imm0 1)
+    (la imm0 target::misc-data-offset imm0))
+  #+pp32-target
+  (progn
+    (la imm0 target::misc-data-offset arg_x))
+  (li imm1 target::misc-data-offset)
+  (bge @have-scaled-table-index)
+  (lwzx imm1 temp1 imm0)
+  (slri imm1 imm1 target::word-shift)
+  (la imm1 target::misc-data-offset imm1)
+  @have-scaled-table-index
+  (ldrx arg_z temp0 imm1)
+  (blr))
+
+(defppclapfunction %small-slot-id-value ((instance arg_y) (slot-id arg_z))
+  (ldr temp1 'map nfn)
+  (svref arg_x slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (ldr temp0 'table nfn)
+  (header-length imm3 imm0)
+  (cmplr arg_x imm3)
+  (srri imm0 arg_x target::word-shift)
+  (la imm0 target::misc-data-offset imm0)
+  (bge @missing)
+  (lbzx imm1 temp1 imm0)
+  (cmpri imm1 0)
+  (slri imm1 imm1 target::word-shift)
+  (la imm1 target::misc-data-offset imm1)
+  (beq @missing)
+  @have-scaled-table-index
+  (ldrx arg_z temp0 imm1)
+  (ldr arg_x 'class nfn)
+  (ldr nfn '%maybe-std-slot-value nfn)
+  (ldr temp0 target::misc-data-offset nfn)
+  (set-nargs 3)
+  (mtctr temp0)
+  (bctr)
+  @missing                              ; (%slot-id-ref-missing instance id)
+  (ldr nfn '%slot-id-ref-missing nfn)
+  (set-nargs 2)
+  (ldr temp0 target::misc-data-offset nfn)
+  (mtctr temp0)
+  (bctr))
+
+(defppclapfunction %large-slot-id-value ((instance arg_y) (slot-id arg_z))
+  (ldr temp1 'map nfn)
+  (svref arg_x slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (ldr temp0 'table nfn)
+  (header-length imm3 imm0)
+  (cmplr arg_x imm3)
+  #+ppc64-target
+  (progn
+    (srdi imm0 arg_x 1)
+    (la imm0 target::misc-data-offset imm0))
+  #+ppc32-target
+  (progn
+    (la imm0 target::misc-data-offset arg_x))
+  (bge @missing)
+  (lwzx imm1 temp1 imm0)
+  (cmpri imm1 0)
+  (slri imm1 imm1 target::word-shift)
+  (la imm1 target::misc-data-offset imm1)
+  (beq @missing)
+  @have-scaled-table-index
+  (ldr arg_x 'class nfn)
+  (ldr nfn '%maybe-std-slot-value-using-class nfn)
+  (ldrx arg_z temp0 imm1)
+  (ldr temp0 target::misc-data-offset nfn)
+  (set-nargs 3)
+  (mtctr temp0)
+  (bctr)
+  @missing                              ; (%slot-id-ref-missing instance id)
+  (ldr nfn '%slot-id-ref-missing nfn)
+  (set-nargs 2)
+  (ldr temp0 target::misc-data-offset nfn)
+  (mtctr temp0)
+  (bctr))
+  
+(defppclapfunction %small-set-slot-id-value ((instance arg_x)
+                                             (slot-id arg_y)
+                                             (new-value arg_z))
+  (ldr temp1 'map nfn)
+  (svref imm3 slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (ldr temp0 'table nfn)
+  (header-length imm5 imm0)
+  (cmplr imm3 imm5)
+  (srri imm0 imm3 target::word-shift)
+  (la imm0 target::misc-data-offset imm0)
+  (bge @missing)
+  (lbzx imm1 temp1 imm0)
+  (cmpwi imm1 0)
+  (slri imm1 imm1 target::word-shift)
+  (la imm1 target::misc-data-offset imm1)
+  (beq @missing)
+  @have-scaled-table-index
+  (ldr temp1 'class nfn)
+  (ldrx arg_y temp0 imm1)
+  (ldr nfn '%maybe-std-setf-slot-value-using-class nfn)
+  (set-nargs 4)
+  (ldr temp0 target::misc-data-offset nfn)
+  (vpush temp1)
+  (mtctr temp0)
+  (bctr)
+  @missing                              ; (%slot-id-set-missing instance id new-value)
+  (ldr nfn '%slot-id-ref-missing nfn)
+  (set-nargs 3)
+  (ldr temp0 target::misc-data-offset nfn)
+  (mtctr temp0)
+  (bctr))
+
+(defppclapfunction %large-set-slot-id-value ((instance arg_x)
+                                             (slot-id arg_y)
+                                             (new-value arg_z))
+  (ldr temp1 'map nfn)
+  (svref imm3 slot-id.index slot-id)
+  (getvheader imm0 temp1)
+  (ldr temp0 'table nfn)
+  (header-length imm5 imm0)
+  (cmplw imm3 imm5)
+  #+ppc64-target (srdi imm3 imm3 1)
+  (la imm0 target::misc-data-offset imm3)
+  (bge @missing)
+  (lwzx imm1 temp1 imm0)
+  (cmpwi imm1 0)
+  (la imm1 target::misc-data-offset imm1)
+  (beq @missing)
+  @have-scaled-table-index
+  (ldr temp1 'class nfn)
+  (ldrx arg_y temp0 imm1)
+  (ldr nfn '%maybe-std-setf-slot-value-using-class nfn)
+  (set-nargs 4)
+  (svref temp0 0 nfn)
+  (vpush temp1)
+  (mtctr temp0)
+  (bctr)
+  @missing                              ; (%slot-id-set-missing instance id new-value)
+  (ldr nfn '%slot-id-ref-missing nfn)
+  (set-nargs 3)
+  (ldr temp0 target::misc-data-offset nfn)
+  (mtctr temp0)
+  (bctr))
+
+(defparameter *gf-proto*
+  (nfunction
+   gag
+   (lambda (&lap &lexpr args)
+     (ppc-lap-function 
+      gag 
+      ()
+      (mflr loc-pc)
+      (vpush-argregs)
+      (vpush nargs)
+      (add imm0 vsp nargs)
+      (la imm0 (ash 1 target::word-shift) imm0)                  ; caller's vsp
+      (bla .SPlexpr-entry)
+      (mtlr loc-pc)                     ; return to kernel
+      (mr arg_z vsp)                    ; lexpr
+      (svref arg_y gf.dispatch-table nfn) ; dispatch table
+      (set-nargs 2)
+      (svref nfn gf.dcode nfn)		; dcode function
+      (ldr temp0 target::misc-data-offset nfn)
+      (mtctr temp0)
+      (bctr)))))
+
+(defppclapfunction funcallable-trampoline ()
+  (svref nfn gf.dcode nfn)
+  (svref temp0 0 nfn)
+  (mtctr temp0)
+  (bctr))
+
+
+(defppclapfunction unset-fin-trampoline ()
+  (mflr loc-pc)
+  (bla .SPheap-rest-arg)                ; cons up an &rest arg, vpush it
+  (vpop arg_z)                          ; whoops, didn't really want to
+  (bla .SPsavecontextvsp)
+  (ldr arg_x '"Funcallable instance ~S was called with args ~s, but has no FUNCALLABLE-INSTANCE-FUNCTION" fn)
+  (mr arg_y fn)
+  (set-nargs 3)
+  (ldr fname 'error fn)
+  (bla .SPrestorecontext)
+  (mtlr loc-pc)
+  (ba .SPjmpsym))
---
+++ level-0/X86/x86-numbers.lisp
@@ -0,0 +1,295 @@
+;-*- Mode: Lisp; Package: CCL -*-
+;;;
+;;;   Copyright (C) 1994-2001 Digitool, Inc
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+
+
+(in-package "CCL")
+
+
+
+(defx86lapfunction %fixnum-signum ((number arg_z))
+  (cmpri :cr0 number '0)
+  (li arg_z '0)
+  (beqlr :cr0)
+  (li arg_z '1)               ; assume positive
+  (bgtlr :cr0)
+  (li arg_z '-1)
+  (single-value-return))
+
+;;; see %logcount (ppc-bignum.lisp)
+(defx86lapfunction %ilogcount ((number arg_z))
+  (let ((arg imm0)
+        (shift imm1)
+        (temp imm2))
+    (unbox-fixnum arg number)
+    (mr. shift arg)
+    (li arg_z 0)
+    (b @test)
+    @next
+    (la temp -1 shift)
+    (and. shift shift temp)
+    (la arg_z '1 arg_z)
+    @test
+    (bne @next)
+    (single-value-return)))
+
+(defx86lapfunction %iash ((number arg_y) (count arg_z))
+  (unbox-fixnum imm1 count)
+  (unbox-fixnum imm0 number)
+  (neg. imm2 imm1)
+  (blt @left)
+  (srar imm0 imm0 imm2)
+  (box-fixnum arg_z imm0)
+  (single-value-return)
+  @left
+  (slr arg_z number imm1)
+  (single-value-return))
+
+(defparameter *double-float-zero* 0.0d0)
+(defparameter *short-float-zero* 0.0s0)
+
+
+
+
+
+(defx86lapfunction %fixnum-intlen ((number arg_z))  
+  (unbox-fixnum imm0 arg_z)
+  (cntlzd. imm1 imm0)
+  (bne @nonneg)
+  (not imm1 imm0)
+  (cntlzd imm1 imm1)
+  @nonneg
+  (subfic imm1 imm1 64)
+  (box-fixnum arg_z imm1)
+  (single-value-return))
+
+
+(defx86lapfunction %double-float-negate! ((src arg_y) (res arg_z))
+  (get-double-float fp0 src)
+  (fneg fp1 fp0)
+  (put-double-float fp1 res)
+  (single-value-return))
+
+
+;;; Non-destructive.
+(defx86lapfunction %short-float-negate ((src arg_z))
+  (get-single-float fp0 src)
+  (fneg fp1 fp0)
+  (put-single-float fp1 arg_z)
+  (single-value-return))
+
+
+;;; Caller guarantees that result fits in a fixnum.
+
+(defx86lapfunction %truncate-double-float->fixnum ((arg arg_z))
+  (get-double-float fp0 arg)
+  (fctidz fp0 fp0)
+  (stdu tsp -32 tsp)
+  (std tsp 8 tsp)
+  (stfd fp0 16 tsp)
+  (ld imm0 16 tsp)
+  (la tsp 32 tsp)
+  (box-fixnum arg_z imm0)  
+  (single-value-return))
+
+
+(defx86lapfunction %truncate-short-float->fixnum ((arg arg_z))
+  (get-single-float fp0 arg)
+  (fctidz fp0 fp0)
+  (stdu tsp -32 tsp)
+  (std tsp 8 tsp)
+  (stfd fp0 16 tsp)
+  (ld imm0 16 tsp)
+  (la tsp 32 tsp)
+  (box-fixnum arg_z imm0)  
+  (single-value-return))
+
+;;; DOES round to even
+
+(defx86lapfunction %round-nearest-double-float->fixnum ((arg arg_z))
+  (get-double-float fp0 arg)
+  (fctid fp0 fp0)
+  (stdu tsp -32 tsp)
+  (std tsp 8 tsp)
+  (stfd fp0 16 tsp)
+  (ld imm0 16 tsp)
+  (la tsp 32 tsp)
+  (box-fixnum arg_z imm0)  
+  (single-value-return))
+
+
+(defx86lapfunction %round-nearest-short-float->fixnum ((arg arg_z))
+  (get-single-float fp0 arg)
+  (fctid fp0 fp0)
+  (stdu tsp -32 tsp)
+  (std tsp 8 tsp)
+  (stfd fp0 16 tsp)
+  (ld imm0 16 tsp)
+  (la tsp 32 tsp)
+  (box-fixnum arg_z imm0)  
+  (single-value-return))
+
+
+
+
+
+(defx86lapfunction %fixnum-truncate ((dividend arg_y) (divisor arg_z))
+  (let ((unboxed-quotient imm0)
+        (unboxed-dividend imm1)
+        (unboxed-divisor imm2)
+        (unboxed-product imm3)
+        (product temp0)
+        (boxed-quotient temp1)
+        (remainder temp2))
+    (unbox-fixnum unboxed-dividend dividend)
+    (unbox-fixnum unboxed-divisor divisor)
+    (divdo. unboxed-quotient unboxed-dividend unboxed-divisor)          ; set OV if divisor = 0
+    (box-fixnum boxed-quotient unboxed-quotient)
+    (mulld unboxed-product unboxed-quotient unboxed-divisor)
+    (bns+ @ok)
+    (mtxer rzero)
+    (save-lisp-context)
+    (set-nargs 3)
+    (load-constant arg_x 'truncate)
+    (call-symbol divide-by-zero-error)
+    @not-0
+    @ok
+    (subf imm0 unboxed-product unboxed-dividend)
+    (vpush boxed-quotient)
+    (box-fixnum remainder imm0)
+    (vpush remainder)
+    (set-nargs 2)
+    (la temp0 '2 vsp)
+    (ba .SPvalues)))
+
+
+(defx86lapfunction called-for-mv-p ()
+  (ref-global imm0 ret1valaddr)
+  (ldr imm1 target::lisp-frame.savelr sp)
+  (eq->boolean arg_z imm0 imm1 imm0)
+  (single-value-return))
+  
+
+
+
+
+
+
+
+#|
+Date: Mon, 3 Feb 1997 10:04:08 -0500
+To: info-mcl@digitool.com, wineberg@franz.scs.carleton.ca
+From: dds@flavors.com (Duncan Smith)
+Subject: Re: More info on the random number generator
+Sender: owner-info-mcl@digitool.com
+Precedence: bulk
+
+The generator is a Linear Congruential Generator:
+
+   X[n+1] = (aX[n] + c) mod m
+
+where: a = 16807  (Park&Miller recommend 48271)
+       c = 0
+       m = 2^31 - 1
+
+See: Knuth, Seminumerical Algorithms (Volume 2), Chapter 3.
+
+The period is: 2^31 - 2  (zero is excluded).
+
+What makes this generator so simple is that multiplication and addition mod
+2^n-1 is easy.  See Knuth Ch. 4.3.2 (2nd Ed. p 272).
+
+    ab mod m = ...
+
+If         m = 2^n-1
+           u = ab mod 2^n
+           v = floor( ab / 2^n )
+
+    ab mod m = u + v                   :  u+v < 2^n
+    ab mod m = ((u + v) mod 2^n) + 1   :  u+v >= 2^n
+
+What we do is use 2b and 2n so we can do arithemetic mod 2^32 instead of
+2^31.  This reduces the whole generator to 5 instructions on the 680x0 or
+80x86, and 8 on the 60x.
+
+-Duncan
+
+|#
+; Use the two fixnums in state to generate a random fixnum >= 0 and < 65536
+; Scramble those fixnums up a bit.
+
+(defun %next-random-seed (state)
+  (let* ((seed (dpb (ldb (byte 16 13) (%svref state 1))
+                    (byte 16 16)
+                    (ldb (byte 16 13) (%svref state 2)))))
+    (multiple-value-bind (seed-low seed-high) (%multiply seed (* 2 48271))
+      (setq seed (logand #xffffffff (+ seed-low seed-high)))
+      (setf (%svref state 1) (ash (ldb (byte 16 16) seed) 13)
+            (%svref state 2) (ash (ldb (byte 16 0) seed) 13))
+      (dpb (ldb (byte 8 0) seed-low) (byte 8 8) (ldb (byte 8 3) seed-high)))))
+
+
+
+;;; n1 and n2 must be positive (esp non zero)
+
+(defx86lapfunction %fixnum-gcd ((n1 arg_y)(n2 arg_z))
+  (let ((temp imm0)
+	(u imm1)
+	(v imm2)
+	(ut0 imm3)
+	(vt0 imm4))
+    (unbox-fixnum u n1)
+    (unbox-fixnum v n2)
+    (neg temp u)
+    (and temp temp u)
+    (cntlzd ut0 temp)
+    (subfic ut0 ut0 63)
+    (neg temp v)
+    (and temp temp v)
+    (cntlzd vt0 temp)
+    (subfic vt0 vt0 63)
+    (cmpw cr2 ut0 vt0)
+    (srd u u ut0)
+    (srd v v vt0)
+    (addi ut0 ut0 ppc64::fixnum-shift)
+    (addi vt0 vt0 ppc64::fixnum-shift)
+    @loop
+    (cmpd cr0 u v)
+    (sld arg_z u ut0)
+    (bgt cr0 @u>v)
+    (blt cr0 @u<v)
+    (blelr cr2)
+    (sld arg_z u vt0)
+    (single-value-return)
+    @u>v
+    (sub u u v)
+    @shiftu
+    (andi. temp u (ash 1 1))
+    (srdi u u 1)
+    (beq cr0 @shiftu)
+    (b @loop)
+    @u<v
+    (sub v v u)
+    @shiftv
+    (andi. temp v (ash 1 1))
+    (srdi v v 1)
+    (beq cr0 @shiftv)
+    (b @loop)))
+    
+
+
+
+; End of ppc-numbers.lisp
