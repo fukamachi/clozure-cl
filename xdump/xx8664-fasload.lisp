59b9dd0b508293b10f6279f50551762889f77f60
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-03-05 13:17:23 +0000

    Closer ...
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3630 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- xdump/xx8664-fasload.lisp
+++ xdump/xx8664-fasload.lisp
@@ -24,77 +24,54 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require "XFASLOAD" "ccl:xdump;xfasload"))
 
-
-(defun xload-x86-lap-word (instruction-form)
-  (uvref (uvref (compile nil
-                         `(lambda (&lap 0)
-                           (x86-lap-function () ((?? 0))
-                            ,instruction-form)))
-                  0) #+x8632-target 0 #+x8664-target 1))
-
-
+(defun xload-x8664-lap-code (instructions)
+  (let* ((f (%define-x86-lap-function nil instructions)))
+    (if (= (typecode f) target::subtag-xfunction)
+      (uvref f 0)
+      f)))
 
 (defparameter *x8664-macro-apply-code*
-  (let* ((code
-          (%define-x86-lap-function
-           nil
-           '((movzwl (% nargs) (% imm0.l))
-             (subq ($ (ash $numx8664argregs x8664::word-shift)) (% imm0))
-             (jle @simple)
-             (movq (% rbp) (@ (% rsp) (% imm0)))
-             (leaq (@ (% rsp) (% imm0)) (% rbp))
-             (movq (% ra0) (@ 8 (% rbp)))
-             (jmp @did-frame)
-             @simple
-             (pushq (% ra0))
-             (pushq (% rbp))
-             (movq (% rsp) (% rbp))
-             @did-frame
-             (call-subprim .SPheap-rest-arg)
-             (pop (% arg_z))
-             (movq (% fname) (% arg_y))
-             (movq ($ '$xnotfun) (% arg_x))
-             (set-nargs 3)
-             (jmp-subprim .SPksignalerr)))))
-    (if (= (typecode code) target::subtag-xfunction)
-      (uvref code 0)
-      code)))
-
-(defun x86-fixup-macro-apply-code ()
-  (let* ((codev *x86-macro-apply-code*))
-    (setf (uvref codev 5)
-          (logior (logand #xffff00000 (uvref *x86-macro-apply-code* 5))
-                  (target-arch-case
-                   (:x8632 (ash $xnotfun x8632::fixnumshift))
-                   (:x8664 (ash $xnotfun x8664::fixnumshift)))))
-    codev))
+  (xload-x8664-lap-code
+   '((movzwl (% nargs) (% imm0.l))
+     (subq ($ (ash $numx8664argregs x8664::word-shift)) (% imm0))
+     (jle @simple)
+     (movq (% rbp) (@ (% rsp) (% imm0)))
+     (leaq (@ (% rsp) (% imm0)) (% rbp))
+     (movq (% ra0) (@ 8 (% rbp)))
+     (jmp @did-frame)
+     @simple
+     (pushq (% ra0))
+     (pushq (% rbp))
+     (movq (% rsp) (% rbp))
+     @did-frame
+     (call-subprim .SPheap-rest-arg)
+     (pop (% arg_z))
+     (movq (% fname) (% arg_y))
+     (movq ($ '$xnotfun) (% arg_x))
+     (set-nargs 3)
+     (jmp-subprim .SPksignalerr))))
 
 
-(defparameter *x86-closure-trampoline-code*
-  (let* ((code '((ba .SPcall-closure))))
-    (make-array (length code)
-                :element-type '(unsigned-byte 32)
-                :initial-contents
-                (mapcar #'xload-ppc-lap-word code))))
+(defun x8664-fixup-macro-apply-code ()
+  *x8664-macro-apply-code*)
+
+
+(defparameter *x8664-closure-trampoline-code*
+  (xload-x8664-lap-code '((jmp-subprim  .SPcall-closure))))
+
 
 
 ;;; For now, do this with a UUO so that the kernel can catch it.
-(defparameter *ppc-udf-code*
-  (let* ((code '((uuo_interr #.arch::error-udf-call 0))))
-    (make-array (length code)
-                :element-type '(unsigned-byte 32)
-                :initial-contents
-                (mapcar #'xload-ppc-lap-word code))))
-
-
+(defparameter *x8664-udf-code*
+  (xload-x8664-lap-code '((uuo-error-udf-call))))
 
 
 (defparameter *x8664-linux-xload-backend*
   (make-backend-xload-info
    :name  :linuxx8664
-   :macro-apply-code-function 'x86-fixup-macro-apply-code
-   :closure-trampoline-code *x86-closure-trampoline-code*
-   :udf-code *x86-udf-code*
+   :macro-apply-code-function 'x8664-fixup-macro-apply-code
+   :closure-trampoline-code *x8664-closure-trampoline-code*
+   :udf-code *x8664-udf-code*
    :default-image-name "ccl:ccl;x86-boot64"
    :default-startup-file-name "level-1.lx64fsl"
    :subdirs '("ccl:level-0;X86;X8664;" "ccl:level-0;X86;")
