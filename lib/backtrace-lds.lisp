be04e48037d9b47033f47f2d0d1415edbaf0f2d3
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-03-20 03:27:22 +0000

    Conditionalize GET-REGISTER-VALUE/SET-REGISTER-VALUE catch frame access.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3866 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lib/backtrace-lds.lisp
+++ lib/backtrace-lds.lisp
@@ -335,14 +335,19 @@
 (defun get-register-value (address last-catch index)
   (if address
     (%fixnum-ref address)
-    (uvref last-catch (+ index target::catch-frame.save-save7-cell))))
+    (uvref last-catch (+ index
+                         #+ppc-target target::catch-frame.save-save7-cell
+                         #+x86-target target::catch-frame.save-save3-cell))))
 
 ; Inverse of get-register-value
 
 (defun set-register-value (value address last-catch index)
   (if address
     (%fixnum-set address value)
-    (setf (uvref last-catch (+ index target::catch-frame.save-save7-cell)) value)))
+    (setf (uvref last-catch (+ index
+                               #+ppc-target target::catch-frame.save-save7-cell
+                               #+x86-target target::catch-frame.save-save3-cell
+                               )) value)))
 
 (defun return-from-nth-frame (n &rest values)
   (apply-in-nth-frame n #'values values))
