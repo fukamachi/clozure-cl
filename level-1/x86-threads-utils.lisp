ccd44bb631d5615e8f7321d4cd59932cedf9211b
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-03-30 09:55:32 +0000

    Nee files.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3942 8bdb8f1d-59ce-db11-9e41-0016172a54ae

---
+++ level-1/x86-threads-utils.lisp
@@ -0,0 +1,60 @@
+;;; x86-trap-support
+;;;
+;;;   Copyright (C) 2005-2006 Clozure Associates and contributors
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+
+(in-package "CCL")
+
+
+(defun %frame-backlink (p &optional context)
+  (cond ((fake-stack-frame-p p)
+         (%fake-stack-frame.next-sp p))
+        ((fixnump p)
+         (let ((backlink (%%frame-backlink p))
+               (fake-frame
+                (if context (bt.fake-frames context) *fake-stack-frames*)))
+           (loop
+             (when (null fake-frame) (return backlink))
+             (when (eq backlink (%fake-stack-frame.sp fake-frame))
+               (return fake-frame))
+             (setq fake-frame (%fake-stack-frame.link fake-frame)))))
+        (t (error "~s is not a valid stack frame" p))))
+
+(defun bottom-of-stack-p (p context)
+  (and (fixnump p)
+       (locally (declare (fixnum p))
+	 (let* ((tcr (if context (bt.tcr context) (%current-tcr)))
+                (vs-area (%fixnum-ref tcr target::tcr.vs-area)))
+	   (not (%ptr-in-area-p p vs-area))))))
+
+
+(defun lisp-frame-p (p context)
+  (or (fake-stack-frame-p p)
+      (locally (declare (fixnum p))
+        (let ((next-frame (%frame-backlink p context)))
+          (when (fake-stack-frame-p next-frame)
+            (setq next-frame (%fake-stack-frame.sp next-frame)))
+          (locally (declare (fixnum next-frame))
+            (if (bottom-of-stack-p next-frame context)
+              (values nil t)
+              ;; For now, but maybe want to skip &lexpr frames.
+              t))))))
+
+
+(defun catch-frame-sp (catch)
+  (uvref catch x8664::catch-frame.rbp-cell))
+
+
+
