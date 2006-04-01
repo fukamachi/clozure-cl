84ceb619e62e38fb3939e524c389f8120a579d97
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-04-01 05:50:46 +0000

    Some functions moved here from gc.c before it was renamed/split.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@3985 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lisp-kernel/memory.c
+++ lisp-kernel/memory.c
@@ -17,6 +17,7 @@
 #include "lisp.h"
 #include "lisp-exceptions.h"
 #include "lisp_globals.h"
+#include "Threads.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <stddef.h>
@@ -246,3 +247,377 @@ zero_page(BytePtr start)
   memset(start, 0, page_size);
 #endif
 }
+
+/* area management */
+
+
+area *
+new_area(BytePtr lowaddr, BytePtr highaddr, area_code code)
+{
+  area *a = (area *) (zalloc(sizeof(area)));
+  if (a) {
+    natural ndnodes = area_dnode(highaddr, lowaddr);
+    a->low = lowaddr;
+    a->high = highaddr;
+    a->active = (code == AREA_DYNAMIC) ? lowaddr : highaddr;
+    a->code = code;
+    a->ndnodes = ndnodes;
+    /* Caller must allocate markbits when allocating heap ! */
+    
+  }
+  return a;
+}
+
+static area *
+add_area_before(area *new_area, area *before)
+{
+  area *before_before = before->pred;
+
+  new_area->pred = before_before;
+  new_area->succ = before;
+  before_before->succ = new_area;
+  before->pred = new_area;
+  return new_area;
+}
+
+/*
+  The active dynamic area comes first.
+  Static areas follow dynamic areas.
+  Stack areas follow static areas.
+  Readonly areas come last.
+*/
+
+/*
+  If we already own the area_lock (or during iniitalization), it's safe
+  to add an area.
+*/
+
+
+void
+add_area_holding_area_lock(area *new_area)
+{
+  area *that = all_areas;
+  int
+    thiscode = (int)(new_area->code),
+    thatcode;
+
+  /* Cdr down the linked list */
+  do {
+    that = that->succ;
+    thatcode = (int)(that->code);
+  } while (thiscode < thatcode);
+  add_area_before(new_area, that);
+}
+
+/*
+  In general, we need to own the area lock before adding an area.
+*/
+void
+add_area(area *new_area, TCR *tcr)
+{
+  LOCK(lisp_global(AREA_LOCK),tcr);
+  add_area_holding_area_lock(new_area);
+  LOCK(lisp_global(AREA_LOCK),tcr);
+}  
+
+/*
+  Search areas "forward" from the header's successor, until
+  an area containing ADDR is found or an area with code < MINCODE
+  is encountered.
+  This walks the area list visiting heaps (dynamic, then static)
+  first, then stacks.
+
+*/
+static area *
+find_area_forward(BytePtr addr, area_code mincode)
+{
+  area *p, *header = all_areas;
+
+  for (p = header->succ; p != header; p = p->succ) {
+    area_code pcode = p->code;
+    if (pcode < mincode) {
+      return NULL;
+    }
+    if (pcode >= AREA_READONLY) {
+      if ((addr >= p->low) &&
+          (addr < p->active)) {
+        return p;
+      }
+    } else {
+      if ((addr >= p->active) &&
+          (addr < p->high)) {
+        return p;
+      }
+    }
+  }
+  return NULL;
+}
+
+static area *
+find_area_backward(BytePtr addr, area_code maxcode)
+{
+  area *p, *header = all_areas;
+
+  for (p = header->pred; p != header; p = p->pred) {
+    area_code pcode = p->code;
+
+    if (pcode > maxcode) {
+      return NULL;
+    }
+    if (pcode >= AREA_READONLY) {
+      if ((addr >= p->low) &&
+          (addr < p->active)) {
+        return p;
+      }
+    } else {
+      if ((addr >= p->active) &&
+          (addr < p->high)) {
+        return p;
+      }
+    }
+  }
+  return NULL;
+}
+
+area *
+area_containing(BytePtr addr)
+{
+  return find_area_forward(addr, AREA_VOID);
+}
+
+area *
+heap_area_containing(BytePtr addr)
+{
+  return find_area_forward(addr, AREA_READONLY);
+}
+
+area *
+stack_area_containing(BytePtr addr)
+{
+  return find_area_backward(addr, AREA_TSTACK);
+}
+
+/*
+  Make everything "younger" than the start of the target area
+  belong to that area; all younger areas will become empty, and
+  the dynamic area will have to lose some of its markbits (they
+  get zeroed and become part of the tenured area's refbits.)
+
+  The active dynamic area must have been "normalized" (e.g., its
+  active pointer must match the free pointer) before this is called.
+
+  If the target area is 'tenured_area' (the oldest ephemeral generation),
+  zero its refbits and update YOUNGEST_EPHEMERAL.
+
+*/
+
+void
+tenure_to_area(area *target)
+{
+  area *a = active_dynamic_area, *child;
+  BytePtr 
+    curfree = a->active,
+    target_low = target->low,
+    tenured_low = tenured_area->low;
+  natural 
+    dynamic_dnodes = area_dnode(curfree, a->low),
+    new_tenured_dnodes = area_dnode(curfree, tenured_area->low);
+  bitvector 
+    refbits = tenured_area->refbits,
+    markbits = a->markbits,
+    new_markbits;
+
+  target->high = target->active = curfree;
+  target->ndnodes = area_dnode(curfree, target_low);
+
+  for (child = target->younger; child != a; child = child->younger) {
+    child->high = child->low = child->active = curfree;
+    child->ndnodes = 0;
+  }
+
+  a->low = curfree;
+  a->ndnodes = area_dnode(a->high, curfree);
+
+  new_markbits = refbits + ((new_tenured_dnodes + (nbits_in_word-1)) >> bitmap_shift);
+  
+  if (target == tenured_area) {
+    zero_bits(refbits, new_tenured_dnodes);
+    lisp_global(OLDEST_EPHEMERAL) = ptr_to_lispobj(curfree);
+  } else {
+    /* Need more (zeroed) refbits & fewer markbits */
+    zero_bits(markbits, ((new_markbits-markbits)<<bitmap_shift));
+  }
+   
+  a->markbits = new_markbits;
+  lisp_global(OLDSPACE_DNODE_COUNT) = area_dnode(curfree, lisp_global(HEAP_START));
+}
+
+
+
+/*
+  Make everything younger than the oldest byte in 'from' belong to 
+  the youngest generation.  If 'from' is 'tenured_area', this means
+  that nothing's ephemeral any more (and OLDEST_EPHEMERAL can be set
+  to 0 to indicate this.)
+  
+  Some tenured_area refbits become dynamic area markbits in the process;
+  it's not necessary to zero them, since the GC will do that.
+*/
+
+void
+untenure_from_area(area *from)
+{
+  if (lisp_global(OLDEST_EPHEMERAL) != 0) {
+    area *a = active_dynamic_area, *child;
+    BytePtr curlow = from->low;
+    natural new_tenured_dnodes = area_dnode(curlow, tenured_area->low);
+    
+    for (child = from; child != a; child = child->younger) {
+      child->low = child->active = child->high = curlow;
+      child->ndnodes = 0;
+    }
+    
+    a->low = curlow;
+    a->ndnodes = area_dnode(a->high, curlow);
+    
+    a->markbits = (tenured_area->refbits) + ((new_tenured_dnodes+(nbits_in_word-1))>>bitmap_shift);
+    if (from == tenured_area) {
+      /* Everything's in the dynamic area */
+      lisp_global(OLDEST_EPHEMERAL) = 0;
+      lisp_global(OLDSPACE_DNODE_COUNT) = 0;
+
+    }
+  }
+}
+
+
+Boolean
+egc_control(Boolean activate, BytePtr curfree)
+{
+  area *a = active_dynamic_area;
+  Boolean egc_is_active = (a->older != NULL);
+
+  if (activate != egc_is_active) {
+    if (curfree != NULL) {
+      a->active = curfree;
+    }
+    if (activate) {
+      LispObj *heap_start = ptr_from_lispobj(lisp_global(HEAP_START));
+
+      a->older = g1_area;
+      tenure_to_area(tenured_area);
+      egc_is_active = true;
+    } else {
+      untenure_from_area(tenured_area);
+      a->older = NULL;
+      egc_is_active = false;
+    }
+  }
+  return egc_is_active;
+}
+
+/*
+  Lisp ff-calls this; it needs to set the active area's active pointer
+  correctly.
+*/
+
+Boolean
+lisp_egc_control(Boolean activate)
+{
+  area *a = active_dynamic_area;
+  return egc_control(activate, (BytePtr) a->active);
+}
+
+
+
+  
+/* Splice the protected_area_ptr out of the list and dispose of it. */
+void
+delete_protected_area(protected_area_ptr p)
+{
+  BytePtr start = p->start;
+  int nbytes = p->nprot;
+  protected_area_ptr *prev = &AllProtectedAreas, q;
+
+  if (nbytes) {
+    UnProtectMemory((LogicalAddress)start, nbytes);
+  }
+  
+  while ((q = *prev) != NULL) {
+    if (p == q) {
+      *prev = p->next;
+      break;
+    } else {
+      prev = &(q->next);
+    }
+  }
+
+  deallocate((Ptr)p);
+}
+
+
+
+
+/* 
+  Unlink the area from all_areas.
+  Unprotect and dispose of any hard/soft protected_areas.
+  If the area has a handle, dispose of that as well.
+  */
+
+void
+condemn_area_holding_area_lock(area *a)
+{
+  void free_stack(void *);
+  area *prev = a->pred, *next = a->succ;
+  Ptr h = a->h;
+  protected_area_ptr p;
+
+  prev->succ = next;
+  next->pred = prev;
+
+  p = a->softprot;
+  if (p) delete_protected_area(p);
+
+  p = a->hardprot;
+
+  if (p) delete_protected_area(p);
+
+  if (h) free_stack(h);
+  deallocate((Ptr)a);
+}
+
+
+
+void
+condemn_area(area *a, TCR *tcr)
+{
+  LOCK(lisp_global(AREA_LOCK),tcr);
+  condemn_area_holding_area_lock(a);
+  LOCK(lisp_global(AREA_LOCK),tcr);
+}
+
+
+
+
+/*
+  condemn an area and all the other areas that can be reached
+  via the area.older & area.younger links.
+  This is the function in the ppc::kernel-import-condemn-area slot,
+  called by free-stack-area
+  */
+void
+condemn_area_chain(area *a, TCR *tcr)
+{
+  area *older;
+
+  LOCK(lisp_global(AREA_LOCK),tcr);
+
+  for (; a->younger; a = a->younger) ;
+  for (;a;) {
+    older = a->older;
+    condemn_area_holding_area_lock(a);
+    a = older;
+  }
+  UNLOCK(lisp_global(AREA_LOCK),tcr);
+}
+
