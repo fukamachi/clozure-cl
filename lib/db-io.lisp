4ed59612d3343b0436d4033f0ecfb5eece1c1252
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-04-07 03:27:12 +0000

    CDB-HASH-MASK happens to be = PPC32::TARGET-MOST-POSITIVE-FIXNUM.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@4082 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lib/db-io.lisp
+++ lib/db-io.lisp
@@ -39,12 +39,14 @@
 
 (defparameter *interface-abi-version* 1)
 
+(defconstant cdb-hash-mask (1- (ash 1 29)))
+
 (defun cdb-hash (buf len)
     (declare (fixnum len))
     (let* ((h 5381))
       (declare (fixnum h))
-      (dotimes (i len (logand h ppc32::target-most-positive-fixnum))
-	(setq h (+ h (the fixnum (logand ppc32::target-most-positive-fixnum (ash h 5)))))
+      (dotimes (i len (logand h cdb-hash-mask))
+	(setq h (+ h (the fixnum (logand cdb-hash-mask (ash h 5)))))
 	(setq h (logxor (the (unsigned-byte 8) (%get-unsigned-byte buf i)) h)))))
 
 (defconstant cdbm-hplist 1000)
