30e7b67359b320e1a36cd6c6e826917dd21a6890
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-04-13 12:43:00 +0000

    Trying to find what's (sometimes) trashing a closure during QUIT; embed a #+x86-target breakpoint in APPLY.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@4238 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/l1-aprims.lisp
+++ level-1/l1-aprims.lisp
@@ -77,6 +77,7 @@
    last argument, appended to the value of the last argument, which must be a
    list."
   (declare (dynamic-extent args))
+  #+x86-target (dbg)
   (cond ((null args)
 	 (apply function arg))
 	((null (cdr args))
