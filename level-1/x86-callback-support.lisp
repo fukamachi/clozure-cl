09e71357d51997da5f7bde7c0fc4bdcd1a33f094
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-04-15 15:20:34 +0000

    Ensure that callback trampolines are allocated in executable memory.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@4264 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/x86-callback-support.lisp
+++ level-1/x86-callback-support.lisp
@@ -17,10 +17,36 @@
 
 (in-package "CCL")
 
+(defglobal *callback-alloc-lock* (make-lock))
 
+;;; 
+(defun %make-executable-page ()
+  (#_mmap (%null-ptr)
+          (#_getpagesize)
+          (logior #$PROT_READ #$PROT_WRITE #$PROT_EXEC)
+          (logior #$MAP_PRIVATE #$MAP_ANON)
+          -1
+          0))
+
+(defglobal *available-bytes-for-callbacks* 0)
+(defglobal *current-callback-page* nil)
+
+(defun reset-callback-storage ()
+  (setq *available-bytes-for-callbacks* (#_getpagesize)
+        *current-callback-page* (%make-executable-page)))
+
+(defun %allocate-callback-pointer (n)
+  (with-lock-grabbed (*callback-alloc-lock*)
+    (when (< *available-bytes-for-callbacks* n)
+      (reset-callback-storage))
+    (decf *available-bytes-for-callbacks* n)
+    (values (%inc-ptr *current-callback-page* *available-bytes-for-callbacks*))))
+
+
+  
 (defun make-callback-trampoline (index &optional monitor-exception-ports)
   (declare (ignorable monitor-exception-ports))
-  (let* ((p (malloc 14))
+  (let* ((p (%allocate-callback-pointer 16))
          (addr #.(subprim-name->offset '.SPcallback)))
     (setf (%get-unsigned-byte p 0) #x41 ; movl $n,%r11d
           (%get-unsigned-byte p 1) #xc7
