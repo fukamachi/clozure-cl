01a1cf23190d0aa18ee41bc7c9839ba3a2fc422a
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-06-08 09:26:08 +0000

    Bump (max) fasl version.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@4768 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- xdump/faslenv.lisp
+++ xdump/faslenv.lisp
@@ -58,7 +58,7 @@
 (defconstant $fasl-epush-bit 7)
 (defconstant $fasl-file-id #xff00)
 (defconstant $fasl-file-id1 #xff01)
-(defconstant $fasl-vers #x47)
+(defconstant $fasl-vers #x48)
 (defconstant $fasl-min-vers #x47)
 (defconstant $faslend #xff)
 (defconstant $fasl-buf-len 2048)
