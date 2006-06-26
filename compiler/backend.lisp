59968754ceda983bfb0801af091e2a035b505588
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-06-26 02:31:25 +0000

    PLATFORM-OS-FREEBSD.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@4813 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- compiler/backend.lisp
+++ compiler/backend.lisp
@@ -31,6 +31,7 @@
 (defconstant platform-os-linux 1)
 (defconstant platform-os-solaris 2)
 (defconstant platform-os-darwin 3)
+(defconstant platform-os-freebsd 4)
 
 (defstruct backend
   (name :a :type keyword)
