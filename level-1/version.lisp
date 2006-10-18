f45605857458f26d8ed6525b30813cc239b5e61c
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-10-18 14:04:18 +0000

    Bump.  Binaries later today ?
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5378 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 1)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "pre-061006")
+(defparameter *openmcl-suffix* "pre-061018")
 (defparameter *openmcl-dev-level*
   #+ppc-target nil
   #+(or freebsdx8664-target darwinx8664-target) "Beta"
