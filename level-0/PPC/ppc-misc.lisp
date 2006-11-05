663eb3512d44e7d8c31660ccecbeef9164d8e877
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-11-05 14:42:56 +0000

    Try to detect the case where %NANOSLEEP's second (by reference) argument contains garbage after an EINTR.  (The L word, again.)
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5482 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-0/PPC/ppc-misc.lisp
+++ level-0/PPC/ppc-misc.lisp
@@ -987,5 +987,26 @@
   (ldr imm0 0 imm0)                     ; may fault
   (str imm0 target::macptr.address dest)
   (blr))
-  
+
+;;; Work around buggy #_nanosleep implementations.
+(defppclapfunction %valid-remaining-timespec-time-p ((seconds arg_y) (ptr arg_z))
+  (unbox-fixnum imm4 seconds)
+  (lis imm1 (ash 1000000000 -16))
+  (macptr-ptr imm0 ptr)
+  (ori imm1 imm1 (logand #xffff 1000000000))
+  (li arg_z nil)
+  (ldr imm3 0 imm0)
+  (cmplr cr2 imm0 imm4)
+  (cmpr cr1 imm3 rzero)
+  (ldr imm2 target::node-size imm0)
+  (cmplr imm2 imm1)
+  (bgt cr4 @done)
+  (blt cr1 @done)
+  (bge @done)
+  (or. imm3 imm3 imm2)
+  (beq @done)
+  (li arg_z t)
+  @done
+  (blr))
+
 ; end of ppc-misc.lisp
