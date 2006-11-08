7a104a9e9bdf5dd9fbf58d5a6bde107dffe65355
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2006-11-08 12:15:28 +0000

    Bump.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5521 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 1)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "pre-061105a")
+(defparameter *openmcl-suffix* "pre-061108a")
 (defparameter *openmcl-dev-level*
   #+ppc-target nil
   #+(or freebsdx8664-target darwinx8664-target) "Beta"
