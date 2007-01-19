3a4bd9184f17daa3b77e9a2cc9d0d8973d35bf97
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-01-19 06:14:47 +0000

    New (so far, empty) files.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5737 8bdb8f1d-59ce-db11-9e41-0016172a54ae

---
+++ lib/ffi-darwinppc64.lisp
@@ -0,0 +1,16 @@
+;;;-*- Mode: Lisp; Package: CCL -*-
+;;;
+;;;   Copyright (C) 2007, Clozure Associates and contributors
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
