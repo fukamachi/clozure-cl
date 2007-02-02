9042631cce8527d26fcd4ec18061e41658fbf3e7
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-02-02 09:17:02 +0000

    Specify the SDK to use.  (Setting "SDK=" will hopefully work with older version of xcode.)
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5861 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- darwin-headers/addressbook/C/populate.sh
+++ darwin-headers/addressbook/C/populate.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-h-to-ffi.sh /System/Library/Frameworks/AddressBook.framework/Headers/AddressBook.h
+rm -rf System Developer
+SDK=/Developer/SDKs/MacOSX10.4u.sdk
+CFLAGS="-isysroot ${SDK}"; export CFLAGS
+h-to-ffi.sh ${SDK}/System/Library/Frameworks/AddressBook.framework/Headers/AddressBook.h
