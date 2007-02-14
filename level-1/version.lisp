f241630131c39c9da99b321eb65a437001187b64
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-02-14 06:36:22 +0000

    Bump.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@5931 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 1)
 (defparameter *openmcl-revision* 0)
-(defparameter *openmcl-suffix* "pre-070211")
+(defparameter *openmcl-suffix* "pre-070214")
 (defparameter *openmcl-dev-level* nil)
 
 (defparameter *openmcl-version* (format nil "~d.~d~@[.~d~]~@[-~a~] (~@[~A: ~]~~A)"
