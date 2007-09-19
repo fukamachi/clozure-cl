28412ba6d7db42f3e03d446c3aeed0009bf7571c
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-09-19 07:52:57 +0000

    FRAME-REQUIRED-ARGS: try to make better.  This is only used by SLIME; it was once used for frame restarting.  Try to make the result reasonable in the SLIME case.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@7255 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- lib/backtrace-lds.lisp
+++ lib/backtrace-lds.lisp
@@ -29,107 +29,45 @@
   #+ppc-target #(save7 save6 save5 save4 save3 save2 save1 save0))
 
 
-;;; Returns five values: (ARGS TYPES NAMES COUNT NCLOSED)
-;;; ARGS is a list of the args supplied to the function
-;;; TYPES is a list of the types of the args.
-;;; NAMES is a list of the names of the args.
-;;; TYPES & NAMES will hae entries only for closed-over,
-;;;       required, & optional args.
-;;; COUNT is the number of known-correct elements of ARGS, or T if
-;;;       they're all correct.
-;;; ARGS will be filled with NIL up to the number of required args to lfun
-;;; NCLOSED is the number of closed-over values that are in the prefix of ARGS
-;;;       If COUNT < NCLOSED, it is not safe to restart the function.
+;;; Returns three values: (ARG-VALUES TYPES NAMES), solely for the benefit
+;;; of the FRAME-ARGUMENTS function in SLIME's swank-openmcl.lisp.
+;;; ARG-VALUES is a list of the values of the args supplied to the function
+;;; TYPES is a list of (for bad historical reasons) strings .describing
+;;;   whether they're "required", "optional", etc.  SLIME only really
+;;;   cares about whether this is equal to "keyword" or not.
+;;; NAMES is a list of symbols which name the args.
 (defun frame-supplied-args (frame lfun pc child context)
   (declare (ignore child))
-  (multiple-value-bind (req opt restp keys allow-other-keys optinit lexprp ncells nclosed)
-      (function-args lfun)
-    (declare (ignore allow-other-keys lexprp ncells))
-    (multiple-value-bind (child-vsp vsp) (vsp-limits frame context)
-      (decf vsp)
-      (let* ((frame-size (- vsp child-vsp))
-             (res nil)
-             (types nil)
-             (names nil))
-        (flet ((push-type&name (cellno)
-                 (multiple-value-bind (type name) (find-local-name cellno lfun pc)
-                   (push type types)
-                   (push name names))))
-          (declare (dynamic-extent #'push-type&name))
-          (if (or
-               (<= frame-size 0))
-            ;; Can't parse the frame, but all but the last 3 args are on the stack
-            (let* ((nargs (+ nclosed req))
-                   (vstack-args (max 0 (min frame-size (- nargs 3)))))
-              (dotimes (i vstack-args)
-                (declare (fixnum i))
-                (push (access-lisp-data vsp) res)
-                (push-type&name i)
-                (decf vsp))
-              (values (nreconc res (make-list (- nargs vstack-args)))
-                      (nreverse types)
-                      (nreverse names)
-                      vstack-args
-                      nclosed))
-            ;; All args were vpushed.
-            (let* ((might-be-rest (> frame-size (+ req opt)))
-                   (rest (and restp might-be-rest (access-lisp-data (- vsp req opt))))
-                   (cellno -1))
-              (declare (fixnum cellno))
-              (when (and keys might-be-rest (null rest))
-                (let ((vsp (- vsp req opt))
-                      (keyvect (lfun-keyvect lfun))
-                      (res nil))
-                  (dotimes (i keys)
-                    (declare (fixnum i))
-                    (when (access-lisp-data (1- vsp)) ; key-supplied-p
-                      (push (aref keyvect i) res)
-                      (push (access-lisp-data vsp) res))
-                    (decf vsp 2))
-                  (setq rest (nreverse res))))
-              (dotimes (i nclosed)
-                (declare (fixnum i))
-                (when (<= vsp child-vsp) (return))
-                (push (access-lisp-data vsp) res)
-                (push-type&name (incf cellno))
-                (decf vsp))
-              (dotimes (i req)
-                (declare (fixnum i))
-                (when (<= vsp child-vsp) (return))
-                (push (access-lisp-data vsp) res)
-                (push-type&name (incf cellno))
-                (decf vsp))
-              (if rest
-                (dotimes (i opt)        ; all optionals were specified
-                  (declare (fixnum i))
-                  (when (<= vsp child-vsp) (return))
-                  (push (access-lisp-data vsp) res)
-                  (push-type&name (incf cellno))
-                  (decf vsp))
-                (let ((offset (+ opt (if restp 1 0) (if keys (+ keys keys) 0)))
-                      (optionals nil))
-                  (dotimes (i opt)      ; some optionals may have been omitted
-                    (declare (fixnum i))
-                    (when (<= vsp child-vsp) (return))
-                    (let ((value (access-lisp-data vsp)))
-                      (if optinit
-                        (if (access-lisp-data (- vsp offset))
-                          (progn
-                            (push value optionals)
-                            (push-type&name (incf cellno))
-                            (return)))
-                        (progn (push value optionals)
-                               (push-type&name (incf cellno))))
-                      (decf vsp)))
-                  (unless optinit
-                    ;; assume that null optionals were not passed.
-                    (while (and optionals (null (car optionals)))
-                      (pop optionals)
-                      (pop types)
-                      (pop names)))
-                  (setq rest (nreconc optionals rest))))
-              (values (nreconc res rest) (nreverse types) (nreverse names)
-                      t nclosed))))))))
+  (let* ((arglist (arglist-from-map lfun))
+         (args (arguments-and-locals context frame lfun pc))
+         (state :required))
+    (collect ((arg-values)
+              (types)
+              (names))
+      (dolist (arg arglist)
+        (if (or (member arg lambda-list-keywords)
+                (eq arg '&lexpr))
+          (setq state arg)
+          (let* ((pair (pop args)))
+            (case state
+              (&lexpr
+               (with-list-from-lexpr (rest (cdr pair))
+                 (dolist (r rest) (arg-values r) (names nil) (types nil)))
+               (return))
+              (&rest
+               (dolist (r (cdr pair)) (arg-values r) (names nil) (types nil))
+               (return))
+              (&key
+               (arg-values arg)
+               (names nil)
+               (types nil)))
+            (let* ((value (cdr pair)))
+              (if (eq value (%unbound-marker))
+                (return))
+              (names (car pair))
+              (arg-values value)
+              (types nil)))))
+      (values (arg-values) (types) (names)))))
 
 ;;; I'm skeptical about a lot of this stuff on the PPC, but if anything it's
 ;;; pretty PPC-specific
