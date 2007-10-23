c9352d4b86205e51c9ba2a7b701bc2352283617f
af <af@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-10-23 14:36:43 +0000

    Make the easygui currency converter demo more robust.
    * No longer break on pushing "Convert" if non-numbers are in
      Amount and Exchange rate, and
    * make the result field non-editable.
    
    
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@7500 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- examples/cocoa/easygui/currency-converter.lisp
+++ examples/cocoa/easygui/currency-converter.lisp
@@ -5,13 +5,13 @@
    (:default-initargs :size (point 383 175)
      :position (point 125 513)
      :title "Currency Converter"
-     :resizable-p t #+not-yet nil
+     :resizable-p nil
      :minimizable-p t))
 
 (defmethod initialize-view :after ((cw converter-window))
   (let ((currency-form (make-instance 'form-view
                           :autosize-cells-p t
-                          :interline-spacing 9d0
+                          :interline-spacing 9.0
                           :position (point 15 70)                          
                           :size (point 353 90)))
         (convert-button (make-instance 'push-button-view
@@ -23,13 +23,18 @@
                  :size (point 353 2))))
     (setf (action convert-button)
           #'(lambda ()
-              (setf (entry-text currency-form 2)
-                    (prin1-to-string
-                     (* (read-from-string (entry-text currency-form 1))
-                        (read-from-string (entry-text currency-form 0)))))))
-    (add-entries currency-form
-                 "Exchange Rate per $1:" "Dollars to Convert:"
-                 "Amount in other Currency:")
+              (let ((exchange-rate (read-from-string
+                                    (entry-text currency-form 1) nil nil))
+                    (amount (read-from-string (entry-text currency-form 0)
+                                              nil nil)))
+                (when (and (numberp exchange-rate) (numberp amount))
+                  (setf (entry-text currency-form 2)
+                        (prin1-to-string (* exchange-rate amount)))))))
+    (setf (editable-p (car (last (add-entries currency-form
+                                              "Exchange Rate per $1:"
+                                              "Dollars to Convert:"
+                                              "Amount in other Currency:"))))
+          nil)
     (add-subviews cw currency-form line convert-button)
     (window-show cw)))
 
