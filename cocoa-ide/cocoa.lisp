646b12eba9a028617d3128f794f8046edb0723ac
rme <rme@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-10-31 02:34:28 +0000

    Move lisp-application-delegate class from cocoa.lisp into new file app-delegate.lisp.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@7556 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- cocoa-ide/cocoa.lisp
+++ cocoa-ide/cocoa.lisp
@@ -72,7 +72,8 @@
 (require "COCOA-GREP")
 (require "COCOA-BACKTRACE")
 (require "COCOA-INSPECTOR")
-(require "PROCESSES-WINDOW")
+(require "APP-DELEGATE")
+
 
 (def-cocoa-default *ccl-directory* :string "" nil #'(lambda (old new)
 						      (when (equal new "") (setq new nil))
@@ -100,100 +101,6 @@
     (if (equalp (last dir 2) '("Contents" "MacOS"))
 	(make-pathname :directory (butlast dir 3))
 	path)))
-
-
-
-;;; The application delegate gets notified of state changes in the
-;;; application object.
-(defclass lisp-application-delegate (ns:ns-object)
-    ()
-  (:metaclass ns:+ns-object))
-
-
-(objc:defmethod (#/applicationWillFinishLaunching: :void)
-    ((self lisp-application-delegate) notification)
-  (declare (ignore notification))
-  (initialize-user-interface))
-
-(objc:defmethod (#/applicationWillTerminate: :void)
-    ((self lisp-application-delegate) notification)
-  (declare (ignore notification))
-  ;; UI has decided to quit; terminate other lisp threads.
-  (prepare-to-quit))
-
-(objc:defmethod (#/newListener: :void) ((self lisp-application-delegate)
-                                        sender)
-  (declare (ignore sender))
-  (#/openUntitledDocumentOfType:display:
-   (#/sharedDocumentController ns:ns-document-controller)
-   #@"Listener"
-   t))
-
-(objc:defmethod (#/showListener: :void) ((self lisp-application-delegate)
-                                        sender)
-  (declare (ignore sender))
-  (let* ((all-windows (#/orderedWindows *NSApp*))
-	 (key-window (#/keyWindow *NSApp*))
-	 (listener-windows ())
-	 (top-listener nil))
-    (dotimes (i (#/count all-windows))
-      (let* ((w (#/objectAtIndex: all-windows i))
-	     (wc (#/windowController w)))
-	(when (eql (#/class wc) hemlock-listener-window-controller)
-	  (push w listener-windows))))
-    (setq listener-windows (nreverse listener-windows))
-    (setq top-listener (car listener-windows))
-    (cond 
-     ((null listener-windows)
-      (#/newListener: self +null-ptr+))
-     ((eql key-window top-listener)
-      ;; The current window is a listener.  If there is more than
-      ;; one listener, bring the rear-most forward.
-      (let* ((w (car (last listener-windows))))
-	(if (eql top-listener w)
-	  (#_NSBeep)
-	  (#/makeKeyAndOrderFront: w +null-ptr+))))
-     (t
-      (#/makeKeyAndOrderFront: top-listener +null-ptr+)))))
-
-(objc:defmethod (#/ensureListener: :void) ((self lisp-application-delegate)
-					   sender)
-  "If no listener exists, create one and bring it to the front without making it the key or main window."
-  (declare (ignore sender))
-  (let ((top-listener-document (#/topListener hemlock-listener-document)))
-    (when (eql top-listener-document +null-ptr+)
-      (let* ((dc (#/sharedDocumentController ns:ns-document-controller))
-	     (wc nil))
-	(setq top-listener-document
-	      (#/makeUntitledDocumentOfType:error: dc #@"Listener" +null-ptr+))
-	(#/addDocument: dc top-listener-document)
-	(#/makeWindowControllers top-listener-document)
-	(setq wc (#/lastObject (#/windowControllers top-listener-document)))
-	(#/orderFront: (#/window wc) +null-ptr+)))))
-
-(defloadvar *processes-window-controller* nil)
-
-(objc:defmethod (#/showProcessesWindow: :void) ((self lisp-application-delegate)
-						sender)
-  (declare (ignore sender))
-  (when (null *processes-window-controller*)
-    (setf *processes-window-controller* (make-instance 'processes-window-controller)))
-  (#/showWindow: *processes-window-controller* self))
-
-(defvar *cocoa-application-finished-launching* (make-semaphore)
-  "Semaphore that's signaled when the application's finished launching ...")
-
-(objc:defmethod (#/applicationDidFinishLaunching: :void)
-    ((self lisp-application-delegate) notification)
-  (declare (ignore notification))
-  (signal-semaphore *cocoa-application-finished-launching*))
-
-(objc:defmethod (#/applicationOpenUntitledFile: :<BOOL>)
-    ((self lisp-application-delegate) app)
-  (when (zerop *cocoa-listener-count*)
-    (#/newListener: self app)
-    t))
-
 
 (defmethod ui-object-do-operation ((o ns:ns-application)
                                    operation
