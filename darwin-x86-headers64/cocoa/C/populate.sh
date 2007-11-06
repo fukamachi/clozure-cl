264489d933227d50f3eb6acba9a03bcd51b8d404
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-11-06 02:25:09 +0000

    Add some more frameworks.  Update the CFLAGS in Cocoa-based frameworks' populate.sh scripts to include CFLAGS appropriate for gcc-5465.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@7597 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- darwin-x86-headers64/cocoa/C/populate.sh
+++ darwin-x86-headers64/cocoa/C/populate.sh
@@ -2,7 +2,7 @@
 # For now, things earlier than the 10.5 sdk are pointless
 rm -rf System Developer usr
 SDK=/Developer/SDKs/MacOSX10.5.sdk
-CFLAGS="-m64 -fobjc-abi-version=2 -isysroot ${SDK}"; export CFLAGS
+CFLAGS="-m64 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"; export CFLAGS
 h-to-ffi.sh ${SDK}/usr/include/objc/objc-runtime.h
 h-to-ffi.sh ${SDK}/usr/include/objc/objc-exception.h
 h-to-ffi.sh ${SDK}/usr/include/objc/Object.h
--- darwin-x86-headers64/gl/C/populate.sh
+++ darwin-x86-headers64/gl/C/populate.sh
@@ -4,8 +4,9 @@ if [ $# -eq 1 ]
 then
 SDK=$1
 fi 
-CFLAGS="-m64 -isysroot ${SDK}"; export CFLAGS
+CFLAGS="-m64 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"; export CFLAGS
 rm -rf System Developer
 h-to-ffi.sh ${SDK}/System/Library/Frameworks/OpenGL.framework/Headers/OpenGL.h
+h-to-ffi.sh ${SDK}/System/Library/Frameworks/OpenGL.framework/Headers/glu.h
 h-to-ffi.sh ${SDK}/System/Library/Frameworks/GLUT.framework/Headers/glut.h
 h-to-ffi.sh ${SDK}/System/Library/Frameworks/AGL.framework/Headers/agl.h
