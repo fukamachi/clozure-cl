edc305e4d421bcfc82d7514e696c1508c11cfea6
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-11-08 06:52:14 +0000

    Try (again) to work around a bug having to do with nanosleep returning EINTR and also exceeding the original sleep time.  (AFAICT, this was fixed a long time ago in Leopard betas, a workaround that seemed to negatively affect Linux was removed a few weeks ago, and the bug is back in in the final version of Leopard, at least on x86-64.)
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/trunk/ccl@7610 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- level-1/l1-lisp-threads.lisp
+++ level-1/l1-lisp-threads.lisp
@@ -51,7 +51,16 @@
           (declare (type (signed-byte 32) result))
           (if (and (< result 0)
                    (eql (%get-errno) (- #$EINTR)))
-            (psetq aptr bptr bptr aptr)
+            ;; x86-64 Leopard bug.
+            (let* ((asec (pref aptr :timespec.tv_sec))
+                   (bsec (pref bptr :timespec.tv_sec)))
+              (if (and (>= bsec 0)
+                       (or (< bsec asec)
+                           (and (= bsec asec)
+                                (< (pref bptr :timespec.tv_nsec)
+                                   (pref aptr :timespec.tv_nsec)))))
+                (psetq aptr bptr bptr aptr)
+                (return)))
             (return)))))))
 
 
