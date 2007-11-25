905247c3bcd2cefeddcd1caea9feb88c65e286e6
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-11-25 12:53:44 +0000

    Bump min fasl version.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/branches/working-0711/ccl@7748 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- xdump/faslenv.lisp
+++ xdump/faslenv.lisp
@@ -59,7 +59,7 @@
 (defconstant $fasl-file-id #xff00)
 (defconstant $fasl-file-id1 #xff01)
 (defconstant $fasl-vers #x50)
-(defconstant $fasl-min-vers #x4e)
+(defconstant $fasl-min-vers #x50)
 (defconstant $faslend #xff)
 (defconstant $fasl-buf-len 2048)
 (defmacro deffaslop (n arglist &body body)
