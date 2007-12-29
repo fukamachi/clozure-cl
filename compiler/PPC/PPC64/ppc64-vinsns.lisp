7b98ce088bb59c0128fdc93f7b9233ff54edfd2e
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2007-12-29 03:22:45 +0000

    ppc64 vinsns: :jump vinsn attribute now means "pc-relative jump" only.
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/branches/working-0711/ccl@7965 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- compiler/PPC/PPC64/ppc64-vinsns.lisp
+++ compiler/PPC/PPC64/ppc64-vinsns.lisp
@@ -3720,7 +3720,7 @@
     (bla ,spno)))
 
 (defmacro define-ppc64-subprim-jump-vinsn ((name &rest other-attrs) spno)
-  `(define-ppc64-vinsn (,name :jump :jumpLR ,@other-attrs) (() ())
+  `(define-ppc64-vinsn (,name :jumpLR ,@other-attrs) (() ())
     (ba ,spno)))
 
 (define-ppc64-subprim-jump-vinsn (restore-interrupt-level) .SPrestoreintlevel)
@@ -3849,7 +3849,7 @@
 ;;; a JUMP (to a possibly unknown destination).  If the destination's
 ;;; really known, it should probably be inlined (stack-cleanup, value
 ;;; transfer & jump ...)
-(define-ppc64-vinsn (throw :jump :jump-unknown) (()
+(define-ppc64-vinsn (throw :jump-unknown) (()
 						 ())
   (bla .SPthrow))
 
