65312abdbd3d0bed51ef47b9275789d9c4089ffa
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2008-01-15 10:49:46 +0000

    reorg
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/branches/experimentation/devel/source@8107 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- darwin-headers/chud/C/populate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This is for CHUD 4.1.x.  No two versions of CHUD install in the
-# same place, or contain the same set of headers
-# A lot of the CHUD headers are missing terminating newlines;
-# I'm not sure how to suppress warnings about that (or why those newlines
-# are missing.)
-h-to-ffi.sh -Wno-endif-labels /System/Library/PrivateFrameworks/CHUD.framework/Headers/CHUDCore.h
