f585a4b838f80142af825c837e6215a2350a7b46
gb <gb@8bdb8f1d-59ce-db11-9e41-0016172a54ae>
2008-01-15 09:28:38 +0000

    relocate
    git-svn-id: http://svn.clozure.com/publicsvn/openmcl/branches/experimentation/devel/source@8088 8bdb8f1d-59ce-db11-9e41-0016172a54ae

--- darwin-x86-headers64/qtkit/C/populate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# For now, things earlier than the 10.5 sdk are pointless
-rm -rf System Developer usr
-SDK=/Developer/SDKs/MacOSX10.5.sdk
-CFLAGS="-m64 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"; export CFLAGS
-h-to-ffi.sh ${SDK}/System/Library/Frameworks/QTKit.framework/Headers/QTKit.h
--- darwin-x86-headers64/webkit/C/populate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SDK=/Developer/SDKs/MacOSX10.5.sdk
-if [ $# -eq 1 ]
-then
-SDK=$1
-fi
-rm -rf System Developer usr
-CFLAGS="-m64 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"; export CFLAGS
-h-to-ffi.sh ${SDK}/System/Library/Frameworks/WebKit.framework/Headers/WebKit.h
